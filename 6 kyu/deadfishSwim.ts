8e909b737556f01ea596c78bf8a5ace08304b917
Michael Vu <vu.michaeltien@gmail.com>
2024-03-19 11:00:06 -0400

    added 6 kyu

---
+++ 6 kyu/deadfishSwim.ts
@@ -0,0 +1,35 @@
+// Write a simple parser that will parse and run Deadfish.
+
+// Deadfish has 4 commands, each 1 character long:
+
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+
+// parse("iiisdoso") => [8, 64]
+
+function parse(data: string): number[] {
+  const result: number[] = [];
+  let counter: number = 0;
+
+  for (let command of data) {
+    switch (command) {
+      case "i":
+        counter++;
+        break;
+      case "d":
+        counter--;
+        break;
+      case "s":
+        counter **= 2;
+        break;
+      case "o":
+        result.push(counter);
+        break;
+    }
+  }
+
+  return result;
+}

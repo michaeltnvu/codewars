74856266e4ad5b53e98dee09afd0251165b81383
Michael Vu <vu.michaeltien@gmail.com>
2024-05-09 00:48:45 -0400

    added 6 kyu

---
+++ 6 kyu/decipherThis.ts
@@ -0,0 +1,17 @@
+// You are given a secret message you need to decipher. Here are the things you need to know to decipher it:
+
+// For each word:
+
+// the second and the last letter is switched (e.g. Hello becomes Holle)
+// the first letter is replaced by its character code (e.g. H becomes 72)
+// there are no special characters used, only letters and spaces
+// words are separated by a single space
+// there are no leading or trailing spaces
+// Examples
+
+// '72olle 103doo 100ya' --> 'Hello good day'
+// '82yade 115te 103o'   --> 'Ready set go'
+
+// export function decipherThis(str: string): string {
+  // have fun!
+// }
